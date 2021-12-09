28197b67c92ad8fcabf31e8426123c87356ca716
jiangyu <rain15639746187@163.com>
2021-12-09 10:32:06 +0800

    精确一次性消费

--- gmall-realtime/src/main/java/com/atguigu/gmall/realtime/util/MyKafkaUtil.java
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/util/MyKafkaUtil.java
@@ -3,10 +3,13 @@ package com.atguigu.gmall.realtime.util;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import javax.annotation.Nullable;
 import java.util.Properties;
 
 public class MyKafkaUtil {
@@ -30,9 +33,41 @@ public class MyKafkaUtil {
     public static FlinkKafkaProducer<String> getKafkaProducer(String topic){
         Properties props = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,KAFKA_SERVER);
+        props.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG,15*60*1000 + "");
 
-        return new FlinkKafkaProducer<String>(KAFKA_SERVER,topic,new SimpleStringSchema());
+        //return new FlinkKafkaProducer<String>(KAFKA_SERVER,topic,new SimpleStringSchema());
 
         //return new FlinkKafkaProducer<String>(topic,new SimpleStringSchema(),props);
+
+        //设置FlinkKafkaProducer.Semantic.EXACTLY_ONCE 真正实现精确一次性消费
+        return new FlinkKafkaProducer<String>(
+                "default_topic",
+                new KafkaSerializationSchema<String>() {
+                    @Override
+                    public ProducerRecord<byte[], byte[]> serialize(String element, @Nullable Long timestamp) {
+                        return new ProducerRecord<>(topic,element.getBytes());
+                    }
+                },
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+        );
+
+
     }
+
+
+    public static <T>FlinkKafkaProducer<T> getKafkaProduceWithSchema(KafkaSerializationSchema<T> kafkaSerializationSchema){
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,KAFKA_SERVER);
+        props.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG,15*60*1000 + "");
+        return new FlinkKafkaProducer<T>(
+                "default_topic",
+                kafkaSerializationSchema,
+                props,
+                FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+        );
+
+    }
+
+
 }
