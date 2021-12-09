84664065c492add3818540c4b3bac9136cbb3f22
jiangyu <rain15639746187@163.com>
2021-12-09 10:34:06 +0800

    update

--- gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwd/BasedbApp.java
+++ gmall-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwd/BasedbApp.java
@@ -41,18 +41,18 @@ import java.util.Properties;
 
 public class BasedbApp {
     public static void main(String[] args) throws Exception {
-        //1.设置流处理环境
+        //TODO 1.设置流处理环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(4);
-        //2.设置检查点相关
-        env.enableCheckpointing(5000L, CheckpointingMode.EXACTLY_ONCE); //保证检查点分界线对齐
+        //TODO 2.设置检查点相关
+        /*env.enableCheckpointing(5000L, CheckpointingMode.EXACTLY_ONCE); //保证检查点分界线对齐
         env.getCheckpointConfig().setCheckpointTimeout(60000L);
-        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
+        //env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION);
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,3000L));
         env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/gmall/ck"));
-        System.setProperty("HADOOP_USER_NAME","atguigu");
+        System.setProperty("HADOOP_USER_NAME","atguigu");*/
 
-       //3.从kafka ods_base_db_m 读取数据
+       //TODO 3.从kafka ods_base_db_m 读取数据
        String topic = "ods_base_db_m";
        String groupId = "base_db_m_group_id";
 
@@ -60,7 +60,7 @@ public class BasedbApp {
 
         DataStreamSource<String> kafkaDS = env.addSource(kafkaSource);
 
-        //4.将json字符串转换为jsonObj
+        //TODO 4.将json字符串转换为jsonObj
         SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaDS.map(new MapFunction<String, JSONObject>() {
             @Override
             public JSONObject map(String s) throws Exception {
@@ -68,7 +68,7 @@ public class BasedbApp {
             }
         });
 
-        //5.ETL
+        //TODO 5.ETL 清洗过滤数据
         SingleOutputStreamOperator<JSONObject> etlDS = jsonObjDS.filter(new FilterFunction<JSONObject>() {
             @Override
             public boolean filter(JSONObject jsonObject) throws Exception {
@@ -82,7 +82,7 @@ public class BasedbApp {
         });
 
 
-        //6.使用flinkCDC读取配置表
+        //TODO 6.1使用flinkCDC读取配置表
         DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
                 .hostname("hadoop102")
                 .port(3306)
@@ -93,36 +93,36 @@ public class BasedbApp {
                 .startupOptions(StartupOptions.initial())  //初次读取时扫描全表 而不是从binlog中读取
                 .deserializer(new MyCustomSchema())
                 .build();
-        //将DebeziumSourceFunction对象添加至数据源构建流对象
+        //TODO 6.2 将DebeziumSourceFunction对象添加至数据源构建流对象
         DataStreamSource<String> tableProcessDS = env.addSource(sourceFunction);
 
-        //创建mapstate描述符  广播状态必须是map结构
+        //TODO 6.3 创建mapstate描述符  广播状态必须是map结构
         MapStateDescriptor<String, TableProcess> mapStateDispatcher = new MapStateDescriptor<>(
                 "tableprocess_broadcastState",
                 Types.STRING,
                 Types.POJO(TableProcess.class)
         );
 
-        //将配置表信息进行广播
+        //TODO 6.4 将配置表信息进行广播
         BroadcastStream<String> configDS = tableProcessDS.broadcast(mapStateDispatcher);
-        //创建维度数据侧输出流标签
+        //TODO 6.5 创建维度数据侧输出流标签
         OutputTag<JSONObject> dim_outputTag = new OutputTag<JSONObject>("dim"){};
 
-        //将etlDS和配置信息流进行连接
+        //TODO 7.将etlDS和配置信息流进行连接
         SingleOutputStreamOperator<JSONObject> realDS = etlDS
                 .connect(configDS)
                 .process(new TableProcessFunction(mapStateDispatcher,dim_outputTag));
 
-        //从侧输出流中获取维度数据
+        //TODO 8. 从侧输出流中获取维度数据
         DataStream<JSONObject> dimDS = realDS.getSideOutput(dim_outputTag);
 
       /*  realDS.print("事实表");
         dimDS.print("维度表");*/
 
-        //不同的维度数据写到phoenix不同的表中需要自定义sinkFunction
+        //TODO 9.不同的维度数据写到phoenix不同的表中需要自定义sinkFunction
         dimDS.addSink(new DimSink());
 
-        //不同的事实数据写到kafka不同的主题中 需要自定义KafkaSerializationSchema<T>对象 重写serialize方法生成produceRecord
+        //TODO 10.不同的事实数据写到kafka不同的主题中 需要自定义KafkaSerializationSchema<T>对象 重写serialize方法生成produceRecord
         realDS.addSink(
                 MyKafkaUtil.getKafkaProduceWithSchema(new KafkaSerializationSchema<JSONObject>() {
                     @Override
